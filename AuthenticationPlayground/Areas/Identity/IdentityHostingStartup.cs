1426ed33eebf0ad16429912f08ed203e41cacfdb
tx-smitht <tx.smiththomas@gmail.com>
2023-03-06 21:24:28 -0700

    Add project files.

---
+++ AuthenticationPlayground/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AuthenticationPlayground.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthenticationPlayground.Areas.Identity.IdentityHostingStartup))]
+namespace AuthenticationPlayground.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
